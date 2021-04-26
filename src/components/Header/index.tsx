c590544c134697633830a5c081509aed4bf63236
grsln <grsln@yandex.ru>
2021-04-27 00:40:12 +0500

    add mobile menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,15 @@ import { Button, Nav, Navbar } from "react-bootstrap";
 import Logo from "./assets/logo.svg";
 import s from "./style.module.scss";
 const Header = () => {
+  const handleToggleClick = (e: React.MouseEvent<HTMLElement>) => {
+    const el = e.target as HTMLSpanElement;
+    const body = document.querySelector("body");
+    if (body && el) {
+      body.style.overflow = el.parentElement?.classList.contains("collapsed")
+        ? "hidden"
+        : "";
+    }
+  };
   return (
     <div className={s.main}>
       <Navbar className={s.navbar} collapseOnSelect expand="xl">
@@ -16,8 +25,9 @@ const Header = () => {
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
           className={`fixed-top ${s.burger}`}
+          onClick={handleToggleClick}
         />
-        <Navbar.Collapse id="responsive-navbar-nav">
+        <Navbar.Collapse id="responsive-navbar-nav" className={s.menu}>
           <Nav className="mr-auto">
             <Nav.Link href="https://www.imumk.ru/pages/about">
               О проекте
